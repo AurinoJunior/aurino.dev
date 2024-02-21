29d6ef671dae853cf5ecaa31b9b02ee5260ee9d3
Aurino Geraldo <aurinoaj@gmail.com>
2024-02-21 15:09:10 -0300

    feat: create theme logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { ThemeProvider } from './components/Theme/ThemeProvider'
+import { ToggleTheme } from './components/Theme/ToggleTheme'
+
 export function App() {
-  return <h1>Nice</h1>
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="auridev-theme">
+      <h1>Nice</h1>
+      <ToggleTheme />
+    </ThemeProvider>
+  )
 }
